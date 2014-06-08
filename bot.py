1a804199b480b577c2fce24186012583290501c7
OMOTO Kenji <doloopwhile@gmail.com>
2014-06-08 21:04:52 +0900

    updated

--- bot.py
+++ bot.py
@@ -6,8 +6,10 @@ import io
 import os
 import random
 import sys
+import traceback
 
 from pyquery import PyQuery as pq
+from twython import Twython
 import requests
 
 MAX_PAGE_TRY = 200
@@ -30,48 +32,61 @@ def full_image_url(img_element):
     return urlunparse(('http',) + p[1:6])
 
 
-def main():
-    from twython import Twython
+class BadWikipediaPageError(ValueError):
+    pass
+
+def post_random_page_image():
     twitter = Twython(
         os.environ['TWITTER_BOT_APP_KEY'],
         os.environ['TWITTER_BOT_APP_SECRET'],
         os.environ['TWITTER_BOT_OAUTH_TOKEN'],
         os.environ['TWITTER_BOT_OAUTH_TOKEN_SECRET'],
     )
-    for _ in range(MAX_PAGE_TRY):
-        d = pq(url=RANDOM_PAGE_URL)
-        title = d('title').text().split('-')[0].strip()
-        if not title:
-            print('title is empty')
 
-        elements = d('li.gallerybox img, .thumb img')
-        elements = [x for x in elements if is_content_element(x)]
+    d = pq(url=RANDOM_PAGE_URL)
+    title = d('title').text().split('-')[0].strip()
+    if not title:
+        raise BadWikipediaPageError("Title is empty")
 
-        if not elements:
-            continue
+    elements = d('li.gallerybox img, .thumb img')
+    elements = [x for x in elements if is_content_element(x)]
 
-        img = random.choice(elements)
-        text = pq(pq(img).parents('.gallerybox, .thumb')).text()
+    if not elements:
+        raise BadWikipediaPageError("No images", title)
 
-        if not text:
-            print('text is empty')
-            continue
+    img = random.choice(elements)
+    text = pq(pq(img).parents('.gallerybox, .thumb')).text()
 
-        r = requests.get(full_image_url(img))
+    if not text:
+        raise BadWikipediaPageError("Text is empty", title)
 
+    r = requests.get(full_image_url(img))
+
+    status = "「{}」の項「{}」\nhttp://ja.wikipedia.org/wiki/{}".format(title, text, quote(title))
+    if len(status) > 140:
+        exceed = len(status) - 140
+        if len(text) - exceed < 5:
+            raise BadWikipediaPageError('text is too long ({}, {})'.format(repr(title), repr(text)))
+        else:
+            text = text[: -(exceed + 1)] + '…'
         status = "「{}」の項「{}」\nhttp://ja.wikipedia.org/wiki/{}".format(title, text, quote(title))
-        if len(status) > 140:
-            exceed = len(status) - 140
-            if len(text) - exceed < 5:
-                print('text is too long ({}, {})'.format(repr(title), repr(text)))
-                continue
-            else:
-                text = text[:- (exceed + 1)] + '…'
-            status = "「{}」の項「{}」\nhttp://ja.wikipedia.org/wiki/{}".format(title, text, quote(title))
 
-        twitter.update_status_with_media(status=status, media=io.BytesIO(r.content))
+    twitter.update_status_with_media(status=status, media=io.BytesIO(r.content))
+    print("Posted", repr(status))
 
-        break
+
+
+def main():
+    for _ in range(MAX_PAGE_TRY):
+        try:
+            r = post_random_page_image()
+        except Exception as e:
+            traceback.print_exc()
+        else:
+            sys.exit(0)
+    else:
+        sys.exit(1)
+
 
 
 if __name__ == '__main__':
